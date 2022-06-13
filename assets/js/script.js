d8f8e776abf1a57b3d60772529591325403cc2e3
Sara <sameads9@gmail.com>
2022-06-13 08:56:35 -0500

    fix links and setup password length prompt and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+var generatePassword = function () {
+  //prompt for password length, must be between 8 and 128 characters
+  var promptLength = window.prompt('Select a password length at least 8 characters and no longer than 128 characters.');
 
+  //validate input
+  if (8 <= promptLength && promptLength <= 128) {
+    console.log("acceptable criteria");
+  } else {
+    window.alert("Select a number that is at least 8 and no greater than 128.");
+    return generatePassword();
+  }
+  //prompt for character types: lowercase, uppercase, numeric, and/or special characters
+  //validate input, one character type MUST be selected
+  //generate password that matches selected criteria
+  //display password in alert or write to page
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
